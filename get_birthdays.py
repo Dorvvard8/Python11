2da6cfb66ab73ccbffb0d989d7f195c84e4a03a4
Dorvvard8 <123178107+Dorvvard8@users.noreply.github.com>
2023-02-08 00:46:26 +0200

    Update get_birthdays.py

--- get_birthdays.py
+++ get_birthdays.py
@@ -2,31 +2,44 @@ from datetime import datetime, timedelta
 
 
 def get_birthdays_per_week(users):
+    result_list = []
     date_now = datetime.now()
     date_plus_week = date_now + timedelta(weeks=1)-timedelta(days=1)
     week_days = ['Monday: ', 'Tuesday: ', 'Wednesday: ',
                  'Thursday: ', 'Friday: ', 'Saturday: ', 'Sunday: ']
     day_now = 7-date_now.weekday()
     monday = date_now.day+day_now
+    k = 0
+    for user_item in users:
+        birthday = user_item['birthday']
+        birthday = birthday.replace(year=2023)
+        if birthday > date_plus_week or birthday < date_now-timedelta(days=1):
+            continue
+        if birthday.weekday() == 6 or birthday.weekday() == 5:
+            birthday = birthday.replace(day=monday)
+        day_week_int = int(birthday.weekday())
+        week_days[day_week_int] = week_days[day_week_int] + \
+            str(user_item['name'] + ', ')
+        k += 1
+    for i in range(5):
+        if week_days[i][-2] != ':':
+            print(week_days[i][:-2])
 
-    users = {key: value.replace(year=2023) for key, value in users.items()}
 
-    users2 = {key: value for key, value in users.items()
-              if value < date_plus_week and value > date_now}
+# users = list()
+# users = [
+#     {'name': 'Sveta', 'birthday': datetime(year=2001, month=2, day=6)},    # Monday
+#     {'name': 'Dima', 'birthday': datetime(year=2001, month=2, day=7)},     # Tuesday
+#     {'name': 'Vasia', 'birthday': datetime(year=2001, month=2, day=8)},    # Wednesday
+#     {'name': 'Kolia', 'birthday': datetime(year=1995, month=2, day=9)},    # Thursday
+#     {'name': 'Maria', 'birthday': datetime(year=1998, month=2, day=9)},    # Thursday
+#     {'name': 'Slava', 'birthday': datetime(year=2004, month=2, day=10)},   # Friday
+#     {'name': 'Galina', 'birthday': datetime(year=2004, month=2, day=11)},  # Saturday
+#     {'name': 'Natali', 'birthday': datetime(year=2005, month=2, day=12)},  # Sunday
+#     {'name': 'Serg', 'birthday': datetime(year=2001, month=2, day=13)},    # Monday
+#     {'name': 'Misha', 'birthday': datetime(year=1997, month=2, day=14)},   # Tuesday
+#     {'name': 'Kostia', 'birthday': datetime(year=2003, month=2, day=15)}   # Wednesday
+# ]
 
-    if date_now.weekday() != 0:
-        users3 = {key: value.replace(day=monday) for key, value in users2.items()
-                  if value.weekday() == 6 or value.weekday() == 5}
-        users2.update(users3)
 
-    users = {key: value.weekday() for key, value in users2.items()
-             if value.weekday() != 6 or value.weekday() != 5}
-
-    for k in range(7):
-        to_print = False
-        for key, value in users.items():
-            if value == k:
-                to_print = True
-                week_days[k] = week_days[k] + key + ', '
-        if to_print:
-            print(week_days[k][:-2])
+get_birthdays_per_week(users)
